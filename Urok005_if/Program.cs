0f051eb4cb79d78a20a7afe77ba394446a53c52d
Балканов Богдан <89509494919@mail.ru>
2023-02-10 02:29:24 +0300

    Индивидуальное приветствие, но  не работает

---
+++ Urok005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя:");
+string username = Console.WriteLine();
+
+if(username.ToLower() == "Маша");
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine("username");
+}
