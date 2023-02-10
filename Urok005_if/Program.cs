c6751f22f23bc79c03e232c25e0b655e64622434
Балканов Богдан <89509494919@mail.ru>
2023-02-10 12:04:12 +0300

    исправил

--- Urok005_if/Program.cs
+++ Urok005_if/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.WriteLine("Введите ваше имя:");
-string username = Console.WriteLine();
+string username = Console.ReadLine();
 
-if(username.ToLower() == "Маша");
+if(username.ToLower()=="Маша")
 {
     Console.WriteLine("Ура, это же Маша!");
 }
 else
 {
-    Console.Write("Привет, ");
-    Console.WriteLine("username");
+    Console.Write( "Привет,");
+    Console.WriteLine(username);
 }
