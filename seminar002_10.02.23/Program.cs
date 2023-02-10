0a6146d0d24c07474083eb77293c74b05a55612d
Балканов Богдан <89509494919@mail.ru>
2023-02-10 11:55:16 +0300

    исправил

---
+++ seminar002_10.02.23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ведите 1 число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите 2 число");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine("большее" + numberA); 
+    Console.WriteLine("меньшее" + numberB);
+}
+else
+{
+    Console.WriteLine("большее" + numberB); 
+    Console.WriteLine("меньшее" + numberA);
+}
