61555e08ce45e2c156d572782d37795608e8d202
Балканов Богдан <89509494919@mail.ru>
2023-02-10 10:43:34 +0300

    число в квадрате

---
+++ seminar001_10.02.23/Program.cs
@@ -0,0 +1,3 @@
+﻿int numberA = new Random().Next(1,10); //выводим число из квадрата, только рандомное,квадрат это число умноженное на себя.
+Console.WriteLine(numberA);
+Console.WriteLine(numberA * numberA);
